077ab91685f800d1ee4cd7cebcc8ee493c4a1c1e
Avvai Ketheeswaran <avvai.kethees@gmail.com>
2020-10-18 00:49:41 +0000

    Fixed the table schemas in the database and put in some seed data for users and menu

---
+++ db/schema/01_schema.sql
@@ -0,0 +1,37 @@
+-- Drop and recreate midterm tables
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS menu CASCADE;
+DROP TABLE IF EXISTS orders CASCADE;
+DROP TABLE IF EXISTS menu_orders CASCADE
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  phone_number VARCHAR(255) NOT NULL,
+  is_staff BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE menu (
+  id SERIAL PRIMARY KEY NOT NULL,
+  item_name VARCHAR(255) NOT NULL,
+  description TEXT,
+  price_cents INTEGER NOT NULL,
+  url_photo VARCHAR(255)
+);
+
+
+CREATE TABLE orders (
+  id SERIAL PRIMARY KEY NOT NULL,
+  client_id INTEGER REFERENCES users(id),
+  status VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP,
+  acceoted_at TIMESTAMP,
+  ready_at TIMESTAMP
+);
+
+CREATE TABLE menu_orders (
+  id SERIAL PRIMARY KEY NOT NULL,
+  order_id INTEGER REFERENCES orders(id) NOT NULL,
+  menu_id INTEGER REFERENCES menu(id) NOT NULL,
+  quantity INTEGER NOT NULL
+);
--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,3 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (name, phone_number) VALUES ('Alice', 6135832494);
+
