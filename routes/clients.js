c1b775c050f24218813bcddf392b39291e4b8e9b
Avvai Ketheeswaran <avvai.kethees@gmail.com>
2020-10-19 05:30:26 +0000

    Tried to get database data rendering on the menu page - did not succeed yet ...

--- routes/clients.js
+++ routes/clients.js
@@ -12,8 +12,19 @@ module.exports = (db) => {
 
   // menu order page
   router.get("/order", (req, res) => {
+  //      db.query(`SELECT * FROM menu;`)
+  //     .then(data => {
+  //       const menu = data.rows;
+  //       res.json({ menu});
+  //     })
+  //     .catch(err => {
+  //       res
+  //         .status(500)
+  //         .json({ error: err.message });
+  //     });
+  // });
     res.render("client-order");
-  });
+  // });
 
   //individual order page
   router.get("/order/:id", (req, res) => {
