4991ab871990eaff4059d7665e87c94011e8ead6
Joshua Tan <josh.tan.tann@gmail.com>
2020-10-22 13:46:41 -0700

    Refactor function names and some spacing
    Signed-off-by: Joshua Tan <josh.tan.tann@gmail.com>

--- routes/staff.js
+++ routes/staff.js
@@ -3,30 +3,23 @@ const express = require('express');
 const router  = express.Router();
 const moment = require("moment");
 
-
-
 module.exports = (helper) => {
-  
- 
-
   // get route for staff page -  /staff/
   router.get('/', (req, res) => {
-  
-    
-    helper.updateStatusToCompletedOrder();
+    helper.updatePastReadyOrdersToComplete();
     // called active orders function helper
     helper.getActiveOrders()
       .then(orders => {
-      
+
         let returnObj = {'order': orders};
-        
+
         //called past orders function helper
         helper.getInactiveOrders()
         .then(pastOrders => {
-      
+
           returnObj['pastorder'] = pastOrders;
           res.render('staff',returnObj);
-        
+
         });
     })
     .catch(e => {
@@ -38,22 +31,22 @@ module.exports = (helper) => {
 
   // post route for staff page -  /staff/
   router.post('/', (req, res) => {
-    
+
     let orderId = req.body.order_id;
 
     //get length from user
     let timeFromStaff = req.body.completion_time;
-    
-    
 
-    // chnage length in timestamp using current date 
+
+
+    // chnage length in timestamp using current date
     let timeLength = Number(timeFromStaff);
     let myDate = new Date();
     var newDateObj = moment(myDate).add(timeLength, 'm').toDate();
-   
+
 
     // update status and timeFromStaff in order table
-    helper.updateStatusToAcceptedOrder(orderId, newDateObj);
+    helper.updateOrderStatusToAccepted(orderId, newDateObj);
 
     // redirecting to staff
     res.redirect('/staff/');
@@ -62,7 +55,7 @@ module.exports = (helper) => {
     //twilio message
     const accountSid = process.env.TWILIO_ACCOUNT_SID;
     const autheToken = process.env.TWILIO_ATTH_TOKEN;
-    
+
     const client = require('twilio')(accountSid, autheToken);
     client.messages.create({
       body: 'Your order will be ready at' + newDateObj,
@@ -76,16 +69,16 @@ module.exports = (helper) => {
 
 
 
-  // order detail route -  /staff/order/id 
+  // order detail route -  /staff/order/id
   router.get("/order/:id", (req, res) => {
 
     let orderId = req.params.id;
-    
+
     helper.getOrderDetails(orderId)
     .then(details => {
       let detailObject = {detail: details};
-      
-      
+
+
       //res.json(detailObject);
       res.render("staff-order-id", detailObject);
     })
@@ -97,22 +90,3 @@ module.exports = (helper) => {
 
   return router;
 };
-
-
-
-// module.exports = (helper) => {
-//   router.get("/", (req, res) => {
-//     let query = `SELECT * FROM widgets`;
-//     console.log(query);
-//     helper.query(query)
-//       .then(data => {
-//         const widgets = data.rows;
-//         res.json({ widgets });
-//       })
-//       .catch(err => {
-//         res
-//           .status(500)
-//           .json({ error: err.message });
-//       });
-//   });
-// }
