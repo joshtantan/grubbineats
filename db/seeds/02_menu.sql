44d89cfd2406e66788aa42282e278117234ffc08
Avvai Ketheeswaran <avvai.kethees@gmail.com>
2020-10-22 18:26:49 +0000

    Updated menu seed data and fixed styling on pages

--- db/seeds/02_menu.sql
+++ db/seeds/02_menu.sql
@@ -1,41 +1,19 @@
 -- Menu table seeds here (dummy data)
 INSERT INTO menu (item_name, description, price_cents, url_photo)
-VALUES ('Cheese - Goat With Herbs', 'Etiam justo. Etiam pretium iaculis justo.', 8616, 'http://dummyimage.com/111x139.png/dddddd/000000');
+VALUES ('Hot Dog', 'The best hotdog ever.', 500, 'https://i.imgur.com/jG2n7eo.jpg');
 INSERT INTO menu (item_name, description, price_cents, url_photo)
-VALUES ('Juice - Lemon', 'Nam dui.', 2045, 'http://dummyimage.com/233x178.png/5fa2dd/ffffff');
+VALUES ('Coffee', 'The best coffee ever. Only one size.', 300, 'https://i.imgur.com/2HqIX4t.jpg');
 INSERT INTO menu (item_name, description, price_cents, url_photo)
-VALUES ('Pork - Liver', 'Mauris ullamcorper purus sit amet nulla.', 1205, 'http://dummyimage.com/148x200.jpg/cc0000/ffffff');
+VALUES ('Lemon', 'Fresh, sour, best lemon ever.', 200, 'https://i.imgur.com/iz0JIAp.jpg');
 INSERT INTO menu (item_name, description, price_cents, url_photo)
-VALUES ('Vodka - Lemon, Absolut', 'Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam.', 3532, 'http://dummyimage.com/148x146.jpg/5fa2dd/ffffff');
+VALUES ('Cupcake', 'Best cupcake ever. Real buttercream frosting.', 900, 'https://i.imgur.com/EQvZkWD.jpg');
 INSERT INTO menu (item_name, description, price_cents, url_photo)
-VALUES ('Veal - Osso Bucco', 'Vestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue.', 4642, 'http://dummyimage.com/139x123.png/ff4444/ffffff');
+VALUES ('Fries', 'Best fries ever.', 455, 'https://i.imgur.com/AzzyoGY.jpg');
 INSERT INTO menu (item_name, description, price_cents, url_photo)
-VALUES ('Avocado', 'Suspendisse potenti.', 4560, 'http://dummyimage.com/183x246.png/dddddd/000000');
+VALUES ('Eggplant', 'Best eggplant ever.', 625, 'https://i.imgur.com/LCvStnp.jpg');
 INSERT INTO menu (item_name, description, price_cents, url_photo)
-VALUES ('Beer - Original Organic Lager', 'Suspendisse accumsan tortor quis turpis.', 6162, 'http://dummyimage.com/243x228.bmp/5fa2dd/ffffff');
+VALUES ('Burger', 'Best burger ever. Goes well with eggplant', 2240, 'https://i.imgur.com/j7rPGc2.jpg');
 INSERT INTO menu (item_name, description, price_cents, url_photo)
-VALUES ('Cookies - Amaretto', 'Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim.', 4036, 'http://dummyimage.com/128x121.jpg/5fa2dd/ffffff');
+VALUES ('Papaya', 'Juicy juicy papaya.', 735, 'https://i.imgur.com/YFS8h15.jpg');
 INSERT INTO menu (item_name, description, price_cents, url_photo)
-VALUES ('Bread - Raisin Walnut Pull', 'In hac habitasse platea dictumst.', 3182, 'http://dummyimage.com/133x111.jpg/cc0000/ffffff');
-INSERT INTO menu (item_name, description, price_cents, url_photo)
-VALUES ('Wine - White, Riesling, Henry Of', 'Vivamus vel nulla eget eros elementum pellentesque. Quisque porta volutpat erat.', 5494, 'http://dummyimage.com/220x168.bmp/ff4444/ffffff');
-INSERT INTO menu (item_name, description, price_cents, url_photo)
-VALUES ('Pork - Bacon, Double Smoked', 'In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.', 9142, 'http://dummyimage.com/195x120.bmp/dddddd/000000');
-INSERT INTO menu (item_name, description, price_cents, url_photo)
-VALUES ('Tea - Herbal Orange Spice', 'Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci.', 380, 'http://dummyimage.com/149x135.png/dddddd/000000');
-INSERT INTO menu (item_name, description, price_cents, url_photo)
-VALUES ('Apple - Fuji', 'Ut at dolor quis odio consequat varius.', 5897, 'http://dummyimage.com/193x136.bmp/dddddd/000000');
-INSERT INTO menu (item_name, description, price_cents, url_photo)
-VALUES ('Carrots - Purple, Organic', 'In hac habitasse platea dictumst.', 2364, 'http://dummyimage.com/119x102.png/ff4444/ffffff');
-INSERT INTO menu (item_name, description, price_cents, url_photo)
-VALUES ('Ham - Cooked Bayonne Tinned', 'Sed sagittis. Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci.', 3099, 'http://dummyimage.com/221x110.png/dddddd/000000');
-INSERT INTO menu (item_name, description, price_cents, url_photo)
-VALUES ('Ranchero - Primerba, Paste', 'Morbi vel lectus in quam fringilla rhoncus.', 1645, 'http://dummyimage.com/185x175.bmp/dddddd/000000');
-INSERT INTO menu (item_name, description, price_cents, url_photo)
-VALUES ('Onions - Cooking', 'Praesent blandit.', 9469, 'http://dummyimage.com/171x163.jpg/dddddd/000000');
-INSERT INTO menu (item_name, description, price_cents, url_photo)
-VALUES ('Pork - Suckling Pig', 'Nulla justo.', 6255, 'http://dummyimage.com/119x205.bmp/ff4444/ffffff');
-INSERT INTO menu (item_name, description, price_cents, url_photo)
-VALUES ('Wine - Magnotta - Pinot Gris Sr', 'In hac habitasse platea dictumst.', 1525, 'http://dummyimage.com/168x120.bmp/ff4444/ffffff');
-INSERT INTO menu (item_name, description, price_cents, url_photo)
-VALUES ('Bread - Triangle White', 'Aenean lectus.', 2643, 'http://dummyimage.com/150x238.bmp/cc0000/ffffff');
+VALUES ('Icecream', 'Best icecream ever. One flavor only.', 115, 'https://i.imgur.com/soYeNhd.jpg');
