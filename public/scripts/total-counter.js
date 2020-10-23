845437609b45daaf43a42726ec15860ec379cae4
Avvai Ketheeswaran <avvai.kethees@gmail.com>
2020-10-23 03:33:35 +0000

    Cleaned up front end ejs files and fixed some styling on tables

--- public/scripts/total-counter.js
+++ public/scripts/total-counter.js
@@ -1,5 +1,13 @@
-// $(document).ready(function() {
-//   $('.quantity-input').change(function(){
-//     alert ("handler for .change()called ")
-//   })
-// })
+$(document).ready(function() {
+  $('.dish-counter').change(function(){
+    let total = 0;
+
+    $('.dish-counter').each(function () {
+      let quantityOutput = parseFloat($(this).val())
+      let price = parseFloat($(this).attr("id"))
+      total += quantityOutput * price
+     })
+    let totalString = `$${total.toFixed(2)}`
+    $('#total-price').html(totalString)
+  })
+})
